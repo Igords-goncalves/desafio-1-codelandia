2e6edb5be580c10764ce2f821d0e1beb3462913a
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-08 01:40:42 -0300

    feat(elements): creating elements
    Creating elementes child inside an header element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 export function Header() {
     const header = document.createElement('header');
 
-    const div =document.createElement('div')
+    const divContainer = document.createElement('div')
 
-    header.appendChild(div)
+    const divNavegation = document.createElement('div')
+
+    header.appendChild(divContainer).setAttribute('class', 'container')
+
+    divContainer.appendChild(divNavegation).setAttribute('class', 'navegacao')
+
+    const nav = document.createElement('nav')
+
+    divNavegation.appendChild(nav)
+
+    const aMenu1 = document.createElement('a')
+    const aMenu2 = document.createElement('a')
+
+    nav.appendChild(aMenu1).setAttribute('href', '#')
+    nav.appendChild(aMenu2).setAttribute('href', '#')
 
     return header
 }
