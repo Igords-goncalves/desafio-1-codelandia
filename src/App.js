f626504ccfdcb608c1a5163a213694be8a264457
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-10 14:03:26 -0300

    feat(app.js): appending Main.js
    Appending th component App.js in a component App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Header } from "./components/Header.js";
+import { Main } from './components/Main.js'
 
 const root = document.querySelector("body")
 
 root.append(Header())
+root.append(Main())
