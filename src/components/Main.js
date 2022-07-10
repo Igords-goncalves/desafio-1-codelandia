7d829fffb4eb628e878bd639667f5d2b34f94984
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-10 13:52:59 -0300

    feat(main.js): creating main element
    Creating main elements to append child articles and elements after

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+export function Main() {
+    const main = document.createElement('main')
+
+    main.setAttribute('class', 'container conteudo')
+    main.setAttribute('id', 'conteudo')
+
+    // const article = document.createElement('article')
+    
+    // const appedElements = (element, child) => {
+    //     element.appendChild(child)
+    // }
+
+    // appedElements(main, article)
+    // appedElements(main, article)
+
+    return main
+}
