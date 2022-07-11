b3a13d2137fe89d9896b2e3fb4e478d0b4aacd59
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-11 10:20:56 -0300

    refactor(article.js): refactoring titles, news and links
    Replace title, news and links instead a large text in a Artticle.js file

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,5 @@
 import { AppendElements } from "../utils/AppendElements.js";
+import { links, news, titles } from "../utils/NewsAndLinks.js";
 
 export function Article() {
     const article = document.createElement("article");
@@ -24,27 +25,20 @@ export function Article() {
 
     AppendElements(article, span);
 
-    const textTitle = "Kurama morta e Sasuske sem hinegan";
     const h2 = document.createElement("h2");
     h2.setAttribute("class", "title");
-    h2.insertAdjacentText("beforeend", textTitle);
+    h2.insertAdjacentText("beforeend", titles.title1);
 
     const pText = document.createElement("p");
     pText.setAttribute("class", "description");
 
-    const textNews = `O parceiro de Naruto, Kurama - a 
-    raposa de nove caudas, morreu no capítulo 55 do mangá 
-    Boruto: Naruto Next Generations devido ao uso excessivo de 
-    chakra quando Naruto e Kurama usaram o modo Baryon contra Isshiki Ohtsutsuki..`;
     AppendElements(span, h2);
     AppendElements(span, pText);
-    pText.insertAdjacentText("beforeend", textNews);
-
-    const linkUrl = `https://pt.epicdope.com/boruto-cap%C3%ADtulo-55-kurama-est%C3%A1-morto%2C-internautas-n%C3%A3o-est%C3%A3o-felizes/#:~:text=O%20parceiro%20de%20Naruto%2C%20Kurama,modo%20Baryon%20contra%20Isshiki%20Ohtsutsuki.&text=Naruto%20fica%20chocado%20e%20totalmente%20devastado%20pela%20implica%C3%A7%C3%A3o%20de%20Kurama.`;
+    pText.insertAdjacentText("beforeend", news.news1);
 
     const link = document.createElement("a");
     link.setAttribute("class", "font");
-    link.setAttribute("href", linkUrl);
+    link.setAttribute("href", links.link1);
     link.setAttribute("target", "Blank");
     link.insertAdjacentText("beforeend", "jbox.com.br");
     AppendElements(span, link);
