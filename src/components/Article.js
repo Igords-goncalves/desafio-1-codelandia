a5425ee74ac5a49e37bf49ba9d3d702a43146058
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-11 10:53:53 -0300

    refactor(article.js): refactoring code
    Refactoring code and suplain Article.js through parameters comming from Main.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,6 @@
 import { AppendElements } from "../utils/AppendElements.js";
-import { links, news, titles } from "../utils/NewsAndLinks.js";
 
-export function Article() {
+export function Article(date, title, news, links, shortLink) {
     const article = document.createElement("article");
 
     const div = document.createElement("div");
@@ -17,7 +16,7 @@ export function Article() {
     icon.setAttribute("class", "far fa-heart");
 
     AppendElements(div, p);
-    p.insertAdjacentText("beforeend", "20 de jun, 2021");
+    p.insertAdjacentText("beforeend", date);
 
     AppendElements(div, icon);
 
@@ -27,20 +26,20 @@ export function Article() {
 
     const h2 = document.createElement("h2");
     h2.setAttribute("class", "title");
-    h2.insertAdjacentText("beforeend", titles.title1);
-
+    h2.insertAdjacentText("beforeend", title);
+    
     const pText = document.createElement("p");
     pText.setAttribute("class", "description");
 
     AppendElements(span, h2);
     AppendElements(span, pText);
-    pText.insertAdjacentText("beforeend", news.news1);
+    pText.insertAdjacentText("beforeend", news);
 
     const link = document.createElement("a");
     link.setAttribute("class", "font");
-    link.setAttribute("href", links.link1);
+    link.setAttribute("href", links);
     link.setAttribute("target", "Blank");
-    link.insertAdjacentText("beforeend", "jbox.com.br");
+    link.insertAdjacentText("beforeend", shortLink);
     AppendElements(span, link);
 
     return article;
