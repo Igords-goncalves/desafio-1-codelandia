f3e0efc1fa518b29cfcaa21b8ad8b48220a272b3
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-11 10:18:48 -0300

    feat(newsandlinks.js): creating databasae with titles, news and links

---
+++ src/utils/NewsAndLinks.js
@@ -0,0 +1,27 @@
+export const titles = {
+    title1: "Kurama morta e Sasuske sem hinegan",
+    title2: "Última temporada de SNK com menos episódios",
+    title3: "RE:Zero sucesso absoluto no Japão",
+    title4: "Kimetsu no Yaiba melhor anime de 2019"
+}
+
+export const news = {
+    news1: `O parceiro de Naruto, Kurama - a raposa de nove caudas, morreu no capítulo 55 do mangá Boruto: Naruto Next Generations devido ao uso excessivo de chakra quando Naruto e Kurama usaram o modo Baryon contra Isshiki Ohtsutsuki..`,
+
+    news2: `A temporada final de Attack on Titan (Shingeki no Kyojin) estreou em 7 de dezembro com 2 partes, e pode acabar na segunda parte com apenas 16 episódios, sugere o Anime News Network.`,
+
+    news3: `Re:Zero certamente foi um dos animes mais amados e comentados de 2016. Uma história envolvente, cheia de reviravoltas e personagens carismáticos, fizeram parte da fórmula de sucesso usada pelo estúdio White Fox sob o comando de Masaharu Watanabe na adaptação da história original de Tappei Nagatsuki.`,
+
+    news4: `A edição de 2020 do Anime Awards aconteceu em 15 de fevereiro, e, reunindo votos de um júri pré-selecionado e do público, elegeu os melhores e mais populares animes de 2019.`
+}
+
+export const links = {
+    link1: `https://pt.epicdope.com/boruto-cap%C3%ADtulo-55-kurama-est%C3%A1-morto%2C-internautas-n%C3%A3o-est%C3%A3o-felizes/#:~:text=O%20parceiro%20de%20Naruto%2C%20Kurama,modo%20Baryon%20contra%20Isshiki%20Ohtsutsuki.&text=Naruto%20fica%20chocado%20e%20totalmente%20devastado%20pela%20implica%C3%A7%C3%A3o%20de%20Kurama.`,
+
+
+    link2: `https://www.jbox.com.br/2021/01/03/attack-on-titan-ultima-temporada-pode-ter-apenas-16-episodios-entenda/`,
+
+    link3: `https://www.crunchyroll.com/pt-br/anime-news/2017/03/19-1/rezero-boku-no-hero-e-kimi-no-na-wa-so-os-grandes-vencedores-do-sugoi-japan-award-2017`,
+
+    link4: `https://jovemnerd.com.br/nerdbunker/confira-os-vencedores-do-anime-awards-2020/`
+}
