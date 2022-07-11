017abb86efe8d2f243a4e8187d6d64b8dc03883b
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-11 10:56:11 -0300

    feat(main.js): duplicating components Articles.js
    Reusing elements Article.js in a Main.js and giving parameters from database NewsAndLinks.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import { Article } from "./Article.js"
-import { AppendElements } from "../utils/AppendElements.js"
+import { titles, news, links, shortLink, headerDate } from "../utils/NewsAndLinks.js"
 
 export function Main() {
     const main = document.createElement('main')
@@ -7,7 +7,44 @@ export function Main() {
     main.setAttribute('class', 'container conteudo')
     main.setAttribute('id', 'conteudo')
 
-    main.appendChild(Article())
+    main.appendChild(
+        Article(
+            headerDate.date1,
+            titles.title1,
+            news.news1,
+            links.link1,
+            shortLink.shortLink1
+        )
+    )
 
+    main.appendChild(
+        Article(
+            headerDate.date2,
+            titles.title2,
+            news.news2,
+            links.link2,
+            shortLink.shortLink2
+        )
+    )
+
+    main.appendChild(
+        Article(
+            headerDate.date3,
+            titles.title3,
+            news.news3,
+            links.link3,
+            shortLink.shortLink3
+        )
+    )
+
+    main.appendChild(
+        Article(
+            headerDate.date4,
+            titles.title4,
+            news.news4,
+            links.link4,
+            shortLink.shortLink4
+        )
+    )
     return main
 }
